9f78f9fb9175d69b8295dfa964cb04a18d12479c
Porschefana <32975927+Porschefana@users.noreply.github.com>
2018-10-20 16:33:43 +0200

    Update Pivot.cpp

--- src/Subsystems/Pivot.cpp
+++ src/Subsystems/Pivot.cpp
@@ -16,11 +16,11 @@ Pivot::Pivot() : PIDSubsystem("Pivot", 0.003, 0.00000001, 0.000001)
 {
     frc::LiveWindow::GetInstance()->AddActuator("Pivot", "PIDSubsystem Controller", GetPIDController());
 
-	//On récupère tous les capteurs et controlleurs dont on a besoin
+	//On rÃ©cupÃ¨re tous les capteurs et controlleurs dont on a besoin
     moteur = RobotMap::pivotMoteur;
     encodeur = RobotMap::pivotEncodeur;
 
-    //Initialisation et démarrage du PIDController
+    //Initialisation et dÃ©marrage du PIDController
     GetPIDController()->SetContinuous(false);
 	SetAbsoluteTolerance(20);
 	SetInputRange(-700, 700);
@@ -30,7 +30,7 @@ Pivot::Pivot() : PIDSubsystem("Pivot", 0.003, 0.00000001, 0.000001)
 
 double Pivot::ReturnPIDInput()
 {
-	return encodeur->PIDGet();
+	return encodeur->Get();
 }
 
 void Pivot::UsePIDOutput(double output)
@@ -40,6 +40,6 @@ void Pivot::UsePIDOutput(double output)
 
 void Pivot::InitDefaultCommand()
 {
-	//AjusterPivot() sera appelée si aucune autre commande n'est en cours d'éxécution
+	//AjusterPivot() sera appelÃ©e si aucune autre commande n'est en cours d'Ã©xÃ©cution
 	SetDefaultCommand(new AjusterPivot());
 }
